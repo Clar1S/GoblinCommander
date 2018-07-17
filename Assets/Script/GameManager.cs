6c1b7f09f7720e69e6ca51461598d117aec4b434
GagasW <kurnia.walandri@binus.ac.id>
2018-07-17 20:15:26 +0700

    GameUpload

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	bool gameEnded = false; 
+
+	public float restartDelay = 2f;
+
+	public void CompletedGame (){
+
+			Debug.Log ("Game Completed");
+			finishGame ();
+			Invoke ("finishGame", restartDelay); 
+	
+	}
+
+	public void GameOver (){
+
+		if (gameEnded == false)
+		{
+			gameEnded = true;
+			Debug.Log ("Game Over");
+			restartGame ();
+			Invoke ("restartGame", restartDelay); 
+		}
+	}
+
+
+	void restartGame ()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
+	void finishGame ()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float move;
+	public float jump;
+
+	public CharacterController controller;
+
+	private Vector3 moveDirection;
+	public float gravity;
+
+	public Rigidbody rb;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (controller.isGrounded)
+		{
+			moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+			moveDirection = Vector3.ClampMagnitude (moveDirection, 1);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= move;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jump;
+
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+
+		//Rotate Player
+		transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+
+		//Trigger Game Over
+		if (rb.position.y < 0.1f) {
+			FindObjectOfType<GameManager> ().GameOver ();
+		}
+
+
+
+	}
+	//Exit Game
+	public void exitGame(){
+		if(Input.GetKey("Escape")) {
+			Application.Quit();
+		}
+	}
+}
